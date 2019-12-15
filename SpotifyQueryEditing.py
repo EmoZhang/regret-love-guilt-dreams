ec1ee78ef6a3ed6c15265379dc1d1c5516c96124
EmoZhang <steven756284184@yahoo.com>
2019-12-15 22:50:57 +0800

    database clean. finally

--- SpotifyQueryEditing.py
+++ SpotifyQueryEditing.py
@@ -1,6 +1,7 @@
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 import requests
+import pandas as pd
 import SpotifyQuery
 
 with open('client_1.txt', mode='r', encoding='utf8') as c:
@@ -9,16 +10,20 @@ s = SpotifyQuery.AudioFeatures(client_id, client_secret)
 
 client = MongoClient(host='localhost', port=27017)
 db = client['Billboard']
-
 collection = db['ten_year']
 
-object_ids_str = """"""
+# object_ids_str = """"""
+#
+# object_ids = object_ids_str.split('\n')
+#
+# URIs_str = """"""
+#
+# URIs = [i.replace('spotify:track:', '') for i in URIs_str.split('\n')]
 
-object_ids = object_ids_str.split('\n')
-
-URIs_str = """"""
-
-URIs = [i.replace('spotify:track:', '') for i in URIs_str.split('\n')]
+notes = pd.read_csv('cleaning.csv')
+query_df = notes[notes['note'] == '1'][['_id', 'URI']]
+object_ids = list(query_df['_id'])
+URIs = [i.replace('spotify:track:', '') for i in list(query_df['URI'])]
 
 for idx, track_id in enumerate(URIs):
     track_meta = s.get_a_track(track_id=track_id)
